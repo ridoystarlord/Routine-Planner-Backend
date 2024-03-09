3aa853b32a267d7cdfb8ce218f4fe1bbfe4b3fca
Ridoy <ridoy.starlord@gmail.com>
2024-03-10 00:03:15 +0600

    Added Validation for the schedule creation schema

--- src/app/module/schedule/schedule.routes.ts
+++ src/app/module/schedule/schedule.routes.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import IsAuthorized from '../../middlewares/isAuthorized';
+import ValidateRequest from '../../middlewares/validateRequest';
 import { ScheduleControllers } from './schedule.controllers';
+import { ScheduleValidation } from './schedule.validation';
 
 const router = express.Router();
 
 router.get('/', IsAuthorized, ScheduleControllers.GetAllSchedules);
-router.post('/', IsAuthorized, ScheduleControllers.CreateSchedule);
+router.post(
+  '/',
+  IsAuthorized,
+  ValidateRequest(ScheduleValidation.CrateScheduleSchema),
+  ScheduleControllers.CreateSchedule
+);
 
 export const ScheduleRoutes = router;
---
+++ src/app/module/schedule/schedule.validation.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+const timeSlotSchema = z.object({
+  startTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}$/, { message: 'startTime must be in HH:MM format' }),
+  endTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}$/, { message: 'endTime must be in HH:MM format' }),
+});
+
+const CrateScheduleSchema = z.object({
+  date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'Date must be in YYYY-MM-DD format',
+  }),
+  classes: z.array(timeSlotSchema).default([]), // Ensuring it can be an empty array
+  jobs: z.array(timeSlotSchema).default([]), // Ensuring it can be an empty array
+  studySlots: z.array(timeSlotSchema).default([]), // Ensuring it can be an empty array
+});
+
+export const ScheduleValidation = {
+  CrateScheduleSchema,
+};
