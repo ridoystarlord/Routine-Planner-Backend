66bbea202618571e01d9f4f59f17a8db98b28d16
Ridoy <ridoy.starlord@gmail.com>
2024-03-10 00:29:47 +0600

    Added Env Example

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,14 +5,11 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
-  SMS_BASE_URL: process.env.SMS_BASE_URL,
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   jwt: {
     secret: process.env.JWT_SECRET,
     expires_in: process.env.JWT_EXPIRES_IN,
-    refresh_secret: process.env.JWT_REFRESH_SECRET,
-    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
   },
 };
